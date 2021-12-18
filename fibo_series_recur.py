3aaaf1c77a6987f7dd1fc68ebd3e9b9eb85ad23b
sandeshvora <sandeshvora96@gmail.com>
2021-12-18 16:55:50 +0530

    second commit

--- fibo_series_recur.py
+++ fibo_series_recur.py
@@ -1,8 +1,7 @@
 # code to find fibonacci series using recursion
-
 n=int(input("Enter a number to display fibo series "))
 
-def fiboSeries(n):
+def fiboSeriess(n):
     if n<0:
         print('Please enter positive integer ')
     elif n==0:
@@ -10,7 +9,7 @@ def fiboSeries(n):
     elif n==1 or n==2:
         return 1
     else:
-        return fiboSeries(n-1) + fiboSeries(n-2)
+        return fiboSeriess(n-1) + fiboSeriess(n-2)
 
 for i in range(n):
-    print(fiboSeries(i))
+    print(fiboSeriess(i))
