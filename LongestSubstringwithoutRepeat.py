57890bd42709414138a239d5b388c6ac4959f614
sandeshvora <sandeshvora96@gmail.com>
2021-12-21 23:04:53 +0530

    commit

---
+++ LongestSubstringwithoutRepeat.py
@@ -0,0 +1,19 @@
+arr="abaacad"
+def LongestSubNoRepeat(arr):
+    m={}
+    left=0
+    right=0
+    ans=0
+    n=len(arr)
+    while(left<n and right<n):
+        element=arr[right]
+        if element in m:
+            left = max(left,m[element]+1)
+        m[element]=right
+        ans=max(ans,right-left+1)
+        right+=1
+    return ans
+
+print(LongestSubNoRepeat(arr))
+
+
