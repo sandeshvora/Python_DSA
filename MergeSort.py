177b197159359201cc4264404251203e03589572
sandeshvora <sandeshvora96@gmail.com>
2021-12-26 16:12:11 +0530

    commit

---
+++ MergeSort.py
@@ -0,0 +1,38 @@
+def MergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+        MergeSort(L)
+        MergeSort(R)
+        i=j=k=0
+        while (i < len(L) and j < len(R)):
+            if L[i] < R[j]:
+                arr[k]=L[i]
+                i+=1
+            else:
+                arr[k]=R[j]
+                j+=1
+            k+=1
+        while i< len(L):
+            arr[k]=L[i]
+            i+=1
+            k+=1
+        while j< len(R):
+            arr[k]=R[j]
+            j+=1
+            k+=1
+
+
+def PrintList(arr):
+    for i in range(len(arr)):
+        print(arr[i],end=" ")
+    print()
+arr=[11,2,4,8,9,55,3]
+print("Before sorting")
+PrintList(arr)
+print("After Sorting")
+MergeSort(arr)
+PrintList(arr)
+
+
--- QuickSort.py
+++ QuickSort.py
@@ -1,5 +1,5 @@
-def Partition(start,end,arr):
-    pivot_index=start
+def Partition(start, end, arr):
+    pivot_index= start
     pivot=arr[pivot_index]
 
     while(start<end):
@@ -7,18 +7,18 @@ def Partition(start,end,arr):
             start+=1
         while(arr[end]>pivot):
             end-=1
-        if (start<end):
+        if(start<end):
             arr[start],arr[end]=arr[end],arr[start]
     arr[end],arr[pivot_index]=arr[pivot_index],arr[end]
     return end
 
 def Quicksort(start,end,arr):
     if(start<end):
-        p =Partition(start,end,arr)
-        Quicksort(start,p-1,arr)
-        Quicksort(p+1,end,arr)
+        p = Partition(start, end, arr)
+        Quicksort(start, p - 1, arr)
+        Quicksort(p + 1, end, arr)
 
 arr=[7,4,1,3,9,24,87,45]
-Quicksort(0,len(arr)-1,arr)
+Quicksort(0, len(arr) - 1, arr)
 print(arr)
 
