5faf6d041338541efc9c7048fcee7cc8bb9a3df3
sandeshvora <sandeshvora96@gmail.com>
2021-12-29 15:39:42 +0530

    commit

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,1 +1,13 @@
 
+#Bubble sort
+
+def BubbleSort(arr):
+    n=len(arr)
+    for i in range(n-1):
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+
+arr=[7,3,9,1,2]
+print(BubbleSort(arr))
--- GCD Formula.py
+++ GCD Formula.py
@@ -1,9 +1,25 @@
-#GCD of two numbers
+#GCD of two numbers using euclidean (modulo) operator -- more efficient
 
 def gcd(a,b):
-    if a==0:
-        return b
-    return gcd(b%a,a)
-
-
+    if b==0:
+        return a
+    return gcd(b,a%b)
 print(gcd(60,48))
+
+
+
+# gcd using euclidean formula (subtract)
+
+def gcdsub(a,b):
+    if (a==0):
+        return b
+    if (b==0):
+        return a
+    #// base case
+    if (a == b):
+        return a
+    if(a>b):
+        return gcd(a-b,a)
+    return gcd(a,b-a)
+print("GCD is",end=" ")
+print(gcdsub(98,56))
