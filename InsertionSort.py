429b5ed8eb2b814ad9eecf7798b193730cfc210c
sandeshvora <sandeshvora96@gmail.com>
2022-01-01 18:44:58 +0530

    commit

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,11 +1,11 @@
 #Insertion Sort
-arr = [7,2,6,9,1]
+arr = [7,6,10,8,9]
 def Insert(arr):
     for i in range(1,len(arr)):
         current=arr[i]
         j=i-1
         while(arr[j]>current and j>=0):
-            arr[j+1]= arr[j]
+            arr[j+1]= arr[j] # Right shift
             j-=1
         arr[j+1]=current
     return arr
---
+++ SelectionSort.py
@@ -0,0 +1,17 @@
+# Selection sort code
+
+def SelectionSort(arr):
+    size=len(arr)
+    for i in range(size-1):
+        min_index = i
+        for j in range(min_index+1,size):
+            if(arr[j]< arr[min_index]):
+                min_index=j
+        if(min_index!=i):
+            arr[i],arr[min_index]=arr[min_index],arr[i]
+    return arr
+
+if __name__ == '__main__':
+    arr=[8,5,1,2,9,4]
+    SelectionSort(arr)
+    print(arr)
