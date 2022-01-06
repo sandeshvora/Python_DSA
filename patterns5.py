20a80e01c6ff3cfd7c29092eb8abe90c452dcbe2
sandeshvora <sandeshvora96@gmail.com>
2022-01-06 09:16:59 +0530

    commit

--- patterns5.py
+++ patterns5.py
@@ -1,4 +1,3 @@
-
 def triangle(n):
     # number of spaces
     k = n - 1
@@ -17,6 +16,20 @@ def triangle(n):
             print("* ", end="")
         # ending line after each row
         print("\r")
-
+    k=n-1
+    for i in range(0, n):
+        # inner loop to handle number spaces
+        # values changing acc. to requirement
+        for j in range(0, k):
+            print(end=" ")
+        # decrementing k after each loop
+        k = k - 1
+        # inner loop to handle number of columns
+        # values changing acc. to outer loop
+        for j in range(0, i + 1):
+            # printing stars
+            print("* ", end="")
+        # ending line after each row
+        print("\r")
 n = 5
 triangle(n)
